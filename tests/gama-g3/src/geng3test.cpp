9de5db53e93bf34dd41edd8b0e68f071b4378b84
Ales Cepek <cepek@gnu.org>
2025-02-06 18:25:49 +0100

    Add XML header and end

--- tests/gama-g3/src/geng3test.cpp
+++ tests/gama-g3/src/geng3test.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <sstream>
 
+// ......................................................  .h
 #include <string>
 #include <gnu_gama/ellipsoids.h>
 
@@ -8,10 +10,14 @@ public:
   GenG3(GNU_gama::Ellipsoid e=GNU_gama::ellipsoid_wgs84);
 
   std::string ellipsoid_caption() const;
-  int         ellipsoid_id() const;
+  std::string ellipsoid_id() const;
+  std::string xml_header() const;
+  std::string xml_end() const;
 
 private:
-  GNU_gama::Ellipsoid ell;
+  GNU_gama::Ellipsoid ell_id;
+
+  std::string text() { return ""; }
 };
 
 
@@ -21,26 +27,57 @@ using std::endl;
 
 int main()
 {
-  cout << "Hello Geng3test!" << endl;
-
   GenG3 geng3;
-  cout << "id = " << geng3.ellipsoid_id() << "  " << geng3.ellipsoid_caption() << "\n";
+  if (0)cout << "# geng3test :   ellipsoid  "
+       << "id = " << geng3.ellipsoid_id() << "   "
+       << geng3.ellipsoid_caption() << "\n";
 
+  cout << geng3.xml_header();
+
+  cout << geng3.xml_end();
   return 0;
 }
 
 
-// ......................................................
-GenG3::GenG3(GNU_gama::Ellipsoid e) : ell(e)//=GNU_gama::ellipsoid_wgs84)
+// ......................................................  .cpp
+GenG3::GenG3(GNU_gama::Ellipsoid e) : ell_id(e)
 {
 }
 
 std::string GenG3::ellipsoid_caption() const
 {
-  return GNU_gama::gama_ellipsoid_caption[ell.id];
+  return GNU_gama::gama_ellipsoid_caption[ell_id.id];
 }
 
-int GenG3::ellipsoid_id() const
+std::string GenG3::ellipsoid_id() const
 {
-  return ell.id;
+  return GNU_gama::gama_ellipsoid_id[ell_id.id];
+}
+
+std::string GenG3::xml_header() const
+{
+  std::ostringstream s;
+  s << "<?xml version=\"1.0\" ?>\n"
+    << "<gnu-gama-data xmlns="
+    << "\"http://www.gnu.org/software/gama/gnu-gama-data\">\n\n";
+
+  s << "<text>" << "geng3test" << "</text>\n\n";
+
+  s << "<g3-model>\n"
+    << "   <constants>\n"
+    << "      <apriori-standard-deviation>" << 10
+    <<        "</apriori-standard-deviation>\n"
+    << "      <confidence-level>" << 0.95 << "</confidence-level>\n"
+    << "      <angular-units-gons/>\n"
+    << "      <ellipsoid>" << ellipsoid_id() << "</ellipsoid>\n"
+    << "   </constants>\n";
+
+
+
+  return s.str();
+}
+
+std::string GenG3::xml_end() const
+{
+  return "\n</g3-model>\n</gnu-gama-data>\n";
 }
