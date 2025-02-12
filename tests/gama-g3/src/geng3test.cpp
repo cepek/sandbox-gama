44b3082813d9cc9c8e1ddd160dcd1b4316ba2676
Ales Cepek <cepek@gnu.org>
2025-02-12 04:46:30 +0100

    Separate geng3test.h from the definition file geng3test.cpp

--- tests/gama-g3/src/geng3test.cpp
+++ tests/gama-g3/src/geng3test.cpp
@@ -1,83 +1,8 @@
+#include "geng3test.h"
+
 #include <iostream>
 #include <sstream>
 #include <set>
-
-// ......................................................  .h
-#include <string>
-#include <vector>
-#include <gnu_gama/ellipsoids.h>
-
-
-using Tokens = std::vector<std::vector<std::string>>;
-using std::cout;
-
-struct gend3point {
-  std::string id;               // point ID
-  double B{0}, L{0}, H{0};      // ellipsoidal coordinates
-  double X{0}, Y{0}, Z{0};      // ... corresponding XZY
-  double dB{0}, dL{0}, dH{0};   // simulated coordinate errors
-  enum Status {
-    fixed, free, constr         // "constrained"
-  } BL_status{fixed}, H_status{fixed};
-};
-
-class GenG3 {
-public:
-  GenG3(GNU_gama::Ellipsoid e=GNU_gama::ellipsoid_wgs84);
-
-  std::string ellipsoid_caption() const;
-  std::string ellipsoid_id() const;
-  std::string xml_header() const;
-  std::string xml_end() const;
-
-  std::string xml_points() const;
-  std::string xml_observations() const;
-
-  std::istream& read(std::istream&);
-  void write()
-  {
-    for (auto t1=tokens.begin(); t1!=tokens.end(); t1++)
-    {
-      std::vector<std::string> record = *t1;
-
-      for (auto i=0; i<record.size(); i++)
-      {
-        //if (i == 0) std::cout << "record  ";
-        std::cout << record[i] << " ";
-      }
-      std::cout << "\n";
-    }
-  }
-
-  std::string example() const;
-  std::string help() const;
-
-private:
-  Tokens tokens;
-
-  GNU_gama::Ellipsoid ell_id;
-
-  std::string text() { return ""; }
-
-  enum point_status_BLH {
-    BLH_undefined = 0,
-    BLH_fixed_fixed, BLH_fixed_free,   BLH_fixed_constr,
-    BLH_free_free,   BLH_free_fixed,   BLH_free_constr,
-    BLH_constr_free, BLH_constr_fixed, BLH_constr_constr
-  };
-
-  struct g3point {
-    point_status_BLH status;
-    std::string id;
-  };
-
-  std::vector<g3point> points;
-};
-
-
-// ......................................................
-
-//#include <set>
 
 using std::cout;
 using std::endl;
